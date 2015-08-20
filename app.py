75b5642734996296d547392a73c548d2d5269963
Olle Palmgren <olle.palmgren@sonymobile.com>
2015-08-20 07:49:46 +0200

    some vpc support

--- app.py
+++ app.py
@@ -4,9 +4,30 @@ import boto3
 
 if __name__ == '__main__':
     
-    print("-----------------")
-
-
+    """
     s3 = boto3.resource('s3')
     for bucket in s3.buckets.all():
         print(bucket.name)
+    """
+
+    elb = boto3.client('elb')
+
+    response = elb.describe_load_balancers()
+    elbs = response["LoadBalancerDescriptions"];
+    while "NextMarker" in response:
+        response = elb.describe_load_balancers(Marker=response["NextMarker"])
+        elbs += response["LoadBalancerDescriptions"];
+
+    ec2 = boto3.resource("ec2")
+    for vpc in ec2.vpcs.all():
+        print("vpc: {}".format(vpc.id))
+        for subnet in vpc.subnets.all():
+            print("subnet: {} ({})".format(subnet.id, subnet.availability_zone))
+            for instance in subnet.instances.all():
+                print("instance: {}".format(instance.id))
+        for elb in [elb for elb in elbs if elb["VPCId"] == vpc.id]:
+            print("ELB: {}".format(elb["LoadBalancerName"]))
+            print("instances:")
+            for instance in elb["Instances"]:
+                print(" - {}".format(instance["InstanceId"])) 
+                
